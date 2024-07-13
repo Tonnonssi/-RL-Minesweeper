6e6c1426aa73610987c57ada8ec65313dc196b19
Jimin <tonnossi@gmail.com>
2024-07-13 23:53:22 +0900

    delete jango

--- game/codes/minesweeper/game/serializers.py
+++
@@ -1,5 +0,0 @@
-# game/serializers.py
-from rest_framework import serializers
-
-class BoardSerializer(serializers.Serializer):
-    board = serializers.ListField(child=serializers.ListField(child=serializers.DictField()))
--- game/codes/minesweeper/game/urls.py
+++
@@ -1,7 +0,0 @@
-# game/urls.py
-from django.urls import path
-from .views import update_board
-
-urlpatterns = [
-    path('update_board/', update_board, name='update_board')
-]
